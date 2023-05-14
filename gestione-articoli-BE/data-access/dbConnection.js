92ecdde14fa30744d9ca343fb197c2a43d489355
Marco De Biase <liquigas00@gmail.com>
2023-05-14 16:05:57 +0200

    add env variables

--- gestione-articoli-BE/data-access/dbConnection.js
+++ gestione-articoli-BE/data-access/dbConnection.js
@@ -1,28 +1,33 @@
 require('dotenv').config();
-const { Client } = require('pg');
+const mysql = require('mysql2')
+const client = mysql.createConnection(process.env.DATABASE_URL)
+// const { Client } = require('pg');
 
-const client = new Client({
-    host: 'localhost',
-    user: 'postgres',
-    port: 5432,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+// const client = new Client({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     port: 5432,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 
-client.connect();
+// client.connect();
 
 // ############### LOGIN #################
 //    |      |      |       |
 //    |      |      |       |
 const getUser = (condition) => new Promise((resolve, reject) => {
-    client.query(`select * from userlogin where username='${condition}'`, (err, res) => {
+    console.log('ENTRA NEL GET_USER ...');
+    client.query(`select * from UserLogin where username='${condition}'`, (err, res) => {
         if (err) {
             reject(err);
             throw console.error(' *** ERROR *** :' + JSON.stringify(err));
         } else {
             console.log('GETTING DATA');
             //console.log(' $$$$$$    GETTING $$$$$$ (' + JSON.stringify(res) + ') OF THE DATA :');
-            resolve(res.rows[0]);
+            console.log('RES DOPO GETTIN DATA ... ', res);
+            resolve(res[0]);
+            //resolve(res.rows[0]);
         }
         client.end;
     })
@@ -47,7 +52,7 @@ const saveArticolo = (values) => new Promise((resolve, reject) => {
 });
 
 const getArticoli = () => new Promise((resolve, reject) => {
-    client.query(`SELECT * FROM articoli`, (err, res) => {
+    client.query(`SELECT * FROM Articoli`, (err, res) => {
         if (err) {
 
             reject(err);
